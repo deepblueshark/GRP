a2695ef55a398bcd089978d1f03a86497bf43387
Muhammad Izzat Syahmi <khcy5mis@nottingham.edu.my>
2017-03-02 14:09:57 +0800

    fixed running time to display table

--- src/ui.R
+++ src/ui.R
@@ -1,58 +1,28 @@
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(
-  fluidPage(
+shinyUI(fluidPage(
+  titlePanel("File Input"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file","Upload the file"),
+      helpText("Default max. file size is 5MB"),
+      tags$hr(),
+      checkboxInput(inputId = 'header', label = 'Header', value = TRUE),
+      checkboxInput(inputId = "stringAsFactors", label = "stringAsFactors", value = FALSE),
+      
+      radioButtons(inputId = 'sep' , label = 'Separator',choices = c(Comma= ',',Semicolon= ';',Tab='\t', Space=' '),selected = ','),
+      
+      h5(helpText("Please specify your range below before uploading the data file")),
+      sliderInput("eps", "eps Range:", min = 1, max = 10, value = c(1,10)),
+      sliderInput("pts", "minPts Range:", min = 1, max = 10, value = c(1,10)),
+      
+      
+      downloadButton('downloadData', 'Save classification'),
+      br(),
+      downloadButton('downloadData2', 'Save cluster')),
     
-    # Application title
-    titlePanel("File Input"),
     
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-      sidebarPanel(
-        "Main",
-        
-        # fileinput() function is used to get the file upload contorl option
-        fileInput("data", 
-                  "Dataset CSV File:",
-                  accept = c(
-                    "text/csv",
-                    ".csv",
-                    "text/comma-separated-vales,text/plain"
-                  )
-         ),
-                 
-         helpText("Default max. file size is 5MB"),
-                 
-        tabPanel(
-          tags$hr(),
-          h5(helpText("Select your parameters")),
-          #numericInput('n', 'min of slider', 1),
-          #numericInput('m', 'Max of slider', 30),
-          sliderInput("epsRange", "eps Range:", min = 1, max = 30, value = c(1,30)),
-          sliderInput("minPtsRange", "minPts Range:", min = 1, max = 30, value = c(1,30))
-        ),
     
-        tabPanel(
-          h5(helpText("Select the read.table parameters below")),
-          checkboxInput(inputId = 'header', label = 'Header', value = TRUE),
-          checkboxInput(inputId = "stringAsFactors", "stringAsFactors", FALSE),
-          br(),
-          radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ',')
-        )
-      ),
-  
-      mainPanel(
-        uiOutput("tb"),
-        dataTableOutput('fcluster')
-        
-        # Use below code if you want the tabset programming in the main panel. 
-        # If so, then tabset will appear when the app loads for the first time.
-        
-        #tabsetPanel(tabPanel("Summary", verbatimTextOutput("sum")),
-        #tabPanel("Data", tableOutput("table")))
-      )
-    
+    mainPanel(
+      uiOutput("tb")
     )
   )
-)
+))
