46f0cc47040dbca9f413afa37079005ef2b4de26
mostafaosama2 <khcy5mom@nottingham.edu.my>
2017-04-16 23:33:02 +0800

    Added instructions and tips for the user in the interface

--- src/ui.R
+++ src/ui.R
@@ -3,21 +3,26 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       fileInput("file","Upload the file"),
-      helpText("Default max. file size is 5MB"),
+      #helpText("Default max. file size is 5MB"),
       tags$hr(),
+      helpText("Choose this option if your first row contains labels"),
       checkboxInput(inputId = 'header', label = 'Header', value = TRUE),
-      checkboxInput(inputId = "stringAsFactors", label = "stringAsFactors", value = FALSE),
+      #checkboxInput(inputId = "stringAsFactors", label = "stringAsFactors", value = FALSE),
       
       radioButtons(inputId = 'sep' , label = 'Separator',choices = c(Comma= ',',Semicolon= ';',Tab='\t', Space=' '),selected = ','),
       
-      h5(helpText("Please specify your range below before uploading the data file")),
-      sliderInput("eps", "eps Range:", min = 1, max = 10, value = c(1,4)),
-      sliderInput("pts", "minPts Range:", min = 1, max = 10, value = c(1,4)),
+      #h5(helpText("Please specify your range below before uploading the data file")),
+      sliderInput("eps", "eps Range:", min = 1, max = 100, value = c(1,50)),
+      sliderInput("pts", "minPts Range:", min = 1, max = 100, value = c(1,50)),
       
-      
+      helpText("Cluster label legend: 0: Noise points"),
+      helpText("Other numbers : Non-noise points "),
+      helpText("Please note that it may take a few seconds until the save window pops up"),
       downloadButton('downloadData', 'Save classification'),
       br(),
-      downloadButton('downloadData2', 'Save cluster')),
+      downloadButton('downloadData2', 'Save cluster'),
+      br(),
+      downloadButton('downloadData3', 'Save top cluster')),
     
     
     
